0c11f6c1cc2a255df52b47ea239e6508f9c0aa3d
madisonstehle <mnstehle3@gmail.com>
2020-10-25 11:51:13 -0700

    wrote outline for project card rendering

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
+import projects from '../assets/projects.json';
+
+import ProjectCard from '../components/ProjectCard.js';
+
 const ProjectsPage = () => {
+    let cardList = [];
+    
+    console.log('projects.length', projects.length)
+
+    if (projects.length > 0) {
+        projects.forEach( (item, idx) => {
+            cardList.push(<ProjectCard key={ idx } project={ item }/>)
+        })
+    }
+
+    console.log('cardList', cardList)
+
     return (
-        <h1>Projects Page</h1>
+        <div id="projects-page">
+            <h1>Projects Page</h1>
+            { cardList }
+        </div>
     )
 }
 
